46dabf922ca40a16362e62ef1e6c0c90d88b4820
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-08 23:54:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ class evamaria(Client):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
-BOT_TOKEN = environ['BOT_TOKEN']
+BOT_TOKEN = environ.get('BOT_TOKEN', '6523606521:AAGb5nE9rLYUsYbJfMHu1iOhD5BnkkHAxsc')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
